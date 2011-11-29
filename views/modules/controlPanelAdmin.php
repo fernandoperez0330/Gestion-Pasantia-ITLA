97296d2e66fe2e21ffde79aaab600c5ed5eae3bf
Jansel <jvra_16@hotmail.com>
2011-11-29 13:56:24 -0600

    link de carreras

--- views/modules/controlPanelAdmin.php
+++ views/modules/controlPanelAdmin.php
@@ -4,6 +4,7 @@
 <div>
     <ul>
         <li><a href="<?=__URLROOT__?>companiesManager.php">Gestion de Empresas</a></li>
+        <li><a href="<?=__URLROOT__?>careersManager.php">Gestion de Carreras</a></li>
     </ul>
 </div>
 
